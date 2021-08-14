3529de17354d843820d1aa34a17494dd5a7be46e
Krunal Rajkotiya <krunal@auberginesolutions.com>
2021-08-14 20:06:23 +0530

    added: shared modules

--- admin-dashboard/src/app/layouts/default/default.module.ts
+++ admin-dashboard/src/app/layouts/default/default.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { DefaultComponent } from './default.component';
 import { DashboardComponent } from 'src/app/modules/dashboard/dashboard.component';
 import { RouterModule } from '@angular/router';
+import { PostsComponent } from 'src/app/modules/posts/posts.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
-  declarations: [DefaultComponent, DashboardComponent],
-  imports: [CommonModule, RouterModule],
+  declarations: [DefaultComponent, DashboardComponent, PostsComponent],
+  imports: [CommonModule, RouterModule, SharedModule],
 })
 export class DefaultModule {}
